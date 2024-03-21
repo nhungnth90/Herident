169e096f3af90063512418c0012141a67f73df3d
nhungnth4 <nhungnth4@vng.com.vn>
2024-03-21 19:09:07 +0700

    update 21/03

--- dist/desktop/js/ui.js
+++ dist/desktop/js/ui.js
@@ -2,56 +2,25 @@
    JS
    ==================================== */
 
+/*--- Toggle ---*/
+// Search
+$('.site-header>.ic-search').on('click', function () {
+    $('.search-form').css("display", "flex").hide().fadeToggle();
+    $(this).css("display", "none");
+})
+
 /*--- Sticky Nav ---*/
 if ($('.site-header').length > 0) {
-   var normalNavTop = $('.site-header').offset().top;
-   var normalNavBottom = $('.site-header').offset().top + $('.site-header').outerHeight();
-   var originalHeight = $('.site-header').height();
-   $(window).scroll(function() {
-       if ($(window).scrollTop() > normalNavBottom) {
-           $('.site-header').css('height', originalHeight + 'px');
-           $('.site-header').addClass('is-sticky');
-       } else {
-           $('.site-header').removeAttr('style');
-           $('.site-header').removeClass('is-sticky');
-       }
-   });
-}
-
-/*--- Audio ---*/
-$(".audio .right .select-voice").click(function (e) {
-    e.preventDefault();
-    $(".audio .right .voice-board").toggle();
-});
-
-$(".audio .play-button").click(function (e) {
-    e.preventDefault();
-    $(".audio .ic-audio-pause").toggleClass("ic-audio-play");
-});
-
-$(".audio .mute-button").click(function (e) {
-    e.preventDefault();
-    $(".audio .ic-audio-volume").toggleClass("ic-audio-volume-slash");
-});
-
-$(".modal-body .ic-eye").click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass("slash");
-});
-
-/*--- Copy Source ---*/
-if ($('.article__source .getlink').length > 0) {
-    $('.article__source .btn').on('click', function () {
-        $('.source-toggle').toggleClass("show");
-    });
-
-    var $temp = $("<input>");
-    var $url = $('.source-toggle a').attr('href');
-    $('.article__source .getlink').on('click', function () {
-        $("body").append($temp);
-        $temp.val($url).select();
-        document.execCommand("copy");
-        $temp.remove();
-        $('.article__source .getlink').html('Đã copy');
+    var normalNavTop = $('.site-header').offset().top;
+    var normalNavBottom = $('.site-header').offset().top + $('.site-header').outerHeight();
+    var originalHeight = $('.site-header').height();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > normalNavBottom) {
+            $('.site-header').css('height', originalHeight + 'px');
+            $('.site-header').addClass('is-sticky');
+        } else {
+            $('.site-header').removeAttr('style');
+            $('.site-header').removeClass('is-sticky');
+        }
     });
 }
